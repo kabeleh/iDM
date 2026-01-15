01524a9923855c34ba53b8fda15c111a468eda40
Kay Lehnert <43334074+kabeleh@users.noreply.github.com>
2026-01-15 21:14:26 +0100

    load GCC, Cython, and mpi4py from modules

--- SLURM/install_cobaya.sh
+++ SLURM/install_cobaya.sh
@@ -13,9 +13,12 @@
 #SBATCH --mail-type END,FAIL
 
 ## Load software environment
+module load GCC
 module load Python
+module load Cython
 module load OpenMPI
 module load OpenBLAS
+module load mpi4py
 
 #Check Python version
 python -c  'import sys; print(sys.version)'
@@ -31,7 +34,7 @@ source my_python-env/bin/activate
 python -m pip install pip --upgrade
 
 #Install MPI for Python from source to ensure compatibility with OpenMPI
-python -m pip install "mpi4py>=3" --upgrade --no-binary :all:
+# python -m pip install "mpi4py>=3" --upgrade --no-binary :all:
 
 #Check mpi4py installation
 srun -n 2 python -c "from mpi4py import MPI, __version__; print(__version__ if MPI.COMM_WORLD.Get_rank() else '')"
@@ -40,7 +43,7 @@ srun -n 2 python -c "from mpi4py import MPI, __version__; print(__version__ if M
 python -c "from numpy import show_config; show_config()" | grep 'mkl\|openblas_info' -A 1
 
 #Reinstall numpy and scipy to ensure they are linked against OpenBLAS
-python -m pip install --force-reinstall numpy scipy --upgrade
+python -m pip install --force-reinstall numpy --upgrade
 
 #Install cobaya and dependencies
 python -m pip install cobaya --upgrade
