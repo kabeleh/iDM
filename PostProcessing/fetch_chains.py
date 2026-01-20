8226dbc225e12e99d4eb7896a6fb004c0685548c
Kay Lehnert <43334074+kabeleh@users.noreply.github.com>
2026-01-20 19:51:03 +0100

    Extended file to fetch chains and available data sets

--- PostProcessing/fetch_chains.py
+++ PostProcessing/fetch_chains.py
@@ -5,12 +5,12 @@ dbe = DatabaseExplorer()
 # Get a list of currently available models and datasets
 models_list = dbe.models
 datasets_list = dbe.datasets
-# # Choose model, dataset and sampling method
-# method = 'ns' # 'ns' for nested sampling, 'mcmc' for MCMC
-# model = "klcdm" # from models_list
-# dataset = "des_y1.joint+planck_2018_CamSpec" # from datasets_list
-# # Download samples chain
-# samples = dbe.download_samples(method, model, dataset)
-# # Download Cobaya and PolyChord run settings
-# info = dbe.download_info(method, model, dataset)
-print(models_list)
+# Choose model, dataset and sampling method
+method = "ns"  # 'ns' for nested sampling, 'mcmc' for MCMC
+model = "lcdm"  # from models_list
+dataset = "planck_2018_CamSpec" # from datasets_list
+# Download samples chain
+samples = dbe.download_samples(method, model, dataset)
+# Download Cobaya and PolyChord run settings
+info = dbe.download_info(method, model, dataset)
+print(datasets_list)
